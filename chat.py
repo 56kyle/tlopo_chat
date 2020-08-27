30767db2a20d3256840116b12b21b96ac9545209
Kyle Oliver <43910687+56kyle@users.noreply.github.com>
2020-08-26 21:34:53 -0400

    Update chat.py

--- chat.py
+++ chat.py
@@ -9,11 +9,16 @@ def RGB(r, g, b):
 
 
 def display():
+    chat_region = (40, 753, 400, 200)
+    
     foreground = win32gui.GetForegroundWindow()
     dc = win32gui.GetWindowDC(foreground)
+    
     pirate_win = win32gui.FindWindow(None, "The Legend of Pirates Online [BETA]")
     pirate_dc = win32gui.GetWindowDC(pirate_win)
-    win32gui.BitBlt(dc, 40, 753, 440-40, 953-753, pirate_dc, 40, 753, win32con.SRCCOPY)
+    
+    win32gui.BitBlt(dc, chat_region[0], chat_region[1], chat_region[2], chat_region[3], pirate_dc, chat_region[0], chat_region[1], win32con.SRCCOPY)
+    
     win32gui.ReleaseDC(foreground, dc)
     win32gui.ReleaseDC(pirate_win, pirate_dc)
 
